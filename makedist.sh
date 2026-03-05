f74a3178ef02a99db3f41a4f40d7b4b187cac4eb
Andrea Mazzoleni <amadvance@gmail.com>
2026-03-05 11:52:21 +0100

    Build NSIS with all the other packages

--- makedist.sh
+++ makedist.sh
@@ -7,6 +7,9 @@ if test "x$1" = "x-d"; then
 DEBUG=--enable-debug
 fi
 
+# Ask permission at the start
+sudo echo We are root
+
 make distclean
 
 # Reconfigure (with force) to get the latest revision from git
@@ -20,6 +23,7 @@ if ! make dist; then
 	exit 1
 fi
 
+sh makensis.sh
 sh makeslackdist.sh
 sudo sh makeslackware.sh
 sudo sh makedeb.sh
