8392d0a5b05b56a365c5343945c6c46ac438d65e
Andrea Mazzoleni <amadvance@gmail.com>
2026-06-03 17:54:11 +0200

    If the deamon is terminating kill the potential running task

--- daemon/runner.c
+++ daemon/runner.c
@@ -756,6 +756,12 @@ void runner_done(struct snapraid_state* state)
 
 	state_lock(); /* locking makes helgrind happy in the signal */
 
+	struct snapraid_task* task = state->runner.latest;
+	if (task && task->running && task->pid > 0) {
+		log_msg(LVL_INFO, "terminating helper process pid %" PRIu64 " due to daemon shutdown", (uint64_t)task->pid);
+		os_term(task->pid);
+	}
+
 	/* signal the condition to allow the thread to stop */
 	thread_cond_signal(&state->runner.cond);
 
