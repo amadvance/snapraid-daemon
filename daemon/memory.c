c94d7b07463bcd9feb740f79e4c0a3c43b81f5d4
Andrea Mazzoleni <amadvance@gmail.com>
2026-07-13 23:01:32 +0200

    Move safe memory helper functions to dedicated memory.c and memory.h

---
+++ daemon/memory.c
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Andrea Mazzoleni
+
+#include "os/portable.h"
+
+#include "os/os.h"
+#include "memory.h"
+
+/****************************************************************************/
+/* memory */
+
+void* malloc_nofail(size_t size)
+{
+	void* ptr = malloc(size ? size : 1);
+
+	if (!ptr) {
+		/* LCOV_EXCL_START */
+		os_abort();
+		/* LCOV_EXCL_STOP */
+	}
+
+	return ptr;
+}
+
+void* calloc_nofail(size_t count, size_t size)
+{
+	void* ptr = calloc(count, size);
+
+	if (!ptr) {
+		/* LCOV_EXCL_START */
+		os_abort();
+		/* LCOV_EXCL_STOP */
+	}
+
+	return ptr;
+}
+
+void* realloc_nofail(void* ptr, size_t size)
+{
+	ptr = realloc(ptr, size);
+
+	if (!ptr) {
+		/* LCOV_EXCL_START */
+		os_abort();
+		/* LCOV_EXCL_STOP */
+	}
+
+	return ptr;
+}
+
+char* strdup_nofail(const char* str)
+{
+	char* ptr = strdup(str);
+
+	if (!ptr) {
+		/* LCOV_EXCL_START */
+		os_abort();
+		/* LCOV_EXCL_STOP */
+	}
+
+	return ptr;
+}
+
